3bb15214022bf588e8abd4158e80bfab518bb73f
Greg Bowler <greg@g105b.com>
2026-03-16 17:10:16 +0000

    tidy: refactor job to reduce complexity

--- src/Job.php
+++ src/Job.php
@@ -4,6 +4,7 @@ namespace Gt\Cron;
 use DateTime;
 
 class Job {
+	protected ScriptCommandResolver $scriptCommandResolver;
 	protected Expression $expression;
 	protected string $command;
 	protected bool $hasRun;
@@ -16,6 +17,7 @@ class Job {
 		string $command,
 		ScriptOutputMode $scriptOutputMode = ScriptOutputMode::DISCARD
 	) {
+		$this->scriptCommandResolver = new ScriptCommandResolver();
 		$this->expression = $expression;
 		$this->command = $command;
 		$this->hasRun = false;
@@ -117,7 +119,7 @@ class Job {
 	}
 
 	protected function executeScript():void {
-		$command = $this->resolveScriptCommand();
+		$command = $this->scriptCommandResolver->resolve($this->command);
 		$descriptor = $this->createScriptDescriptor();
 		$pipes = [];
 
@@ -208,70 +210,5 @@ class Job {
 		}
 
 		return "/dev/null";
-	}
-
-	protected function resolveScriptCommand():string {
-		$scriptParts = $this->parseScriptCommand($this->command);
-		if(is_null($scriptParts)) {
-			return $this->command;
-		}
-
-		$script = $this->normaliseScriptName($scriptParts["script"]);
-		if(!$this->isValidScriptName($script)) {
-			return $this->command;
-		}
-
-		$scriptPath = $this->getLocalCronScriptPath($script);
-		if(!is_file($scriptPath)) {
-			return $this->command;
-		}
-
-		return PHP_BINARY
-			. " "
-			. escapeshellarg($scriptPath)
-			. $scriptParts["args"];
-	}
-
-	/** @return null|array{script:string,args:string} */
-	protected function parseScriptCommand(string $command):?array {
-		$matches = [];
-		if(!preg_match(
-			"/^(?P<script>\\S+)(?P<args>\\s.*)?$/",
-			$command,
-			$matches
-		)) {
-			return null;
-		}
-
-		return [
-			"script" => $matches["script"],
-			"args" => $matches["args"] ?? "",
-		];
-	}
-
-	protected function normaliseScriptName(string $script):string {
-		if(substr(strtolower($script), -4) === ".php") {
-			return substr($script, 0, -4);
-		}
-
-		return $script;
-	}
-
-	protected function isValidScriptName(string $script):bool {
-		if(strpos($script, "/") !== false
-		|| strpos($script, "\\") !== false) {
-			return false;
-		}
-
-		return strlen($script) > 0
-			&& preg_match("/^[a-zA-Z0-9._-]+$/", $script);
-	}
-
-	protected function getLocalCronScriptPath(string $script):string {
-		return implode(DIRECTORY_SEPARATOR, [
-			getcwd(),
-			"cron",
-			"$script.php",
-		]);
 	}
 }
